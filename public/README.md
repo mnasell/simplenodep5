59a56697c39b01e2d5ca878500a0b8d6c09562e2
root <root@mail.mnasell.de>
2019-05-31 13:42:23 +0200

    initial commit

---
+++ public/README.md
@@ -0,0 +1,1 @@
+# simplenodep5
---
+++ public/sketch.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const sketch = (p) => {
+
+	p.setup = () => {
+		// Create the canvas
+	    p.createCanvas(p.windowWidth, p.windowHeight);
+		p.noLoop();
+	};
+
+	p.draw = () => {
+		p.background(200);
+
+		// Set colors
+		p.fill(204, 101, 192, 127);
+		p.stroke(127, 63, 120);
+
+		// A rectangle
+		p.rect(40, 120, 120, 40);
+		// An ellipse
+		p.ellipse(240, 240, 80, 80);
+		// A triangle
+		p.triangle(300, 100, 320, 100, 310, 80);
+
+		// A design for a simple flower
+		p.translate(580, 200);
+		p.noStroke();
+		for (var i = 0; i < 10; i ++) {
+			p.ellipse(0, 30, 20, 80);
+			p.rotate(p.PI / 5);
+		}
+	};
+
+	p.windowResized = () => {
+		p.resizeCanvas(p.windowWidth, p.windowHeight);
+	}
+}
+
